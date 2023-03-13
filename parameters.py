635dfa415785329586ea3f2e5efd410965493f9e
Daniel <contact@danielgoldman.us>
2023-03-13 18:47:34 -0400

    Editing for personal testing.

--- parameters.py
+++ parameters.py
@@ -1,8 +1,8 @@
-internal_capacity = 1575
-sub_capacity = 750
+internal_capacity = 1400
+sub_capacity = 700
 subs = 5 # Number of subconscious cycles
-contraction_tolerance = 0.88 # Maximum tolerable reduction in long term memory - more loss is tolerable with higher subs because they store some data
+contraction_tolerance = 0.85 # Maximum tolerable reduction in long term memory - more loss is tolerable with higher subs because they store some data
 thinkpause = 0.1
-subpause = 0.3
+subpause = 0.1
 
-plot = "You are a tour guide of a certain kingdom, in a fantasy world of wonder, magic, and mystery, showing people around the kingdom."
+plot = "Monsters live together underneath a town, with intentions of helping the people but getting misunderstood each time."
